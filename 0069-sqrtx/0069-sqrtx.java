9dfa55319b1542e6c37d5f1bafb6e5cfda645840
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-08-17 23:30:48 +0530

    Time: 0 ms (100%), Space: 40.9 MB (36.21%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+    return (int)Math.sqrt(x);
+    // Code Written by me-    
+        // int sqrt = 0;
+        // for(int i=0; i<=  Math.sqrt(x); i++){
+        //     if(i*i <= x){
+        //         sqrt = i;
+        //         continue;
+        //     }
+        //     else if(i*i > x){
+        //         return sqrt;
+        //     }
+        // }
+        // return sqrt;
+
+        //0-ms code
+    }
+}
