626b92c67af6a42a8f4370115d3fc25b6bb21147
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-08-27 19:04:30 +0530

    Time: 0 ms (100%), Space: 45.6 MB (94.25%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,51 +1,49 @@
 class Solution {
 
-    public static int findFirst(int x, int[] arr){
-        int first=-1;
-        int s=0;
-        int e=arr.length-1;
-        int m=s+(e-s)/2;
+    public static int findFirst(int x, int[] arr) {
+        int first = -1;
+        int s = 0;
+        int e = arr.length - 1;
+        int m = s + (e - s) / 2;
 
-        while(s <= e){
-            m = s+(e-s)/2;
-            if(arr[m] == x){
+        while (s <= e) {
+            m = s + (e - s) / 2;
+            if (arr[m] == x) {
                 first = m;
-                e=m-1;
-            }
-            else if(arr[m] > x){
-                e = m-1;
-            }
-            else s = m+1;
+                e = m - 1;
+            } else if (arr[m] > x) {
+                e = m - 1;
+            } else
+                s = m + 1;
         }
         return first;
     }
-    
-    public static int findLast(int x, int[] arr){
-        int last=-1;
-        int s=0;
-        int e=arr.length-1;
-        int m=s+(e-s)/2;
-        while(s <= e){
-            m = s+(e-s)/2;
-            if(arr[m] == x){
+
+    public static int findLast(int x, int[] arr) {
+        int last = -1;
+        int s = 0;
+        int e = arr.length - 1;
+        int m = s + (e - s) / 2;
+        while (s <= e) {
+            m = s + (e - s) / 2;
+            if (arr[m] == x) {
                 last = m;
-                s=m+1;
-            }
-            else if(arr[m] > x){
-                e = m-1;
-            }
-            else s = m+1;
+                s = m + 1;
+            } else if (arr[m] > x) {
+                e = m - 1;
+            } else
+                s = m + 1;
         }
         return last;
     }
 
     public int[] searchRange(int[] n, int t) {
-        int[] ans = new int[]{-1,-1};
-        int s=0; 
-        int e=n.length-1;
-        int m = s+(e-s)/2;
+        int[] ans = new int[] { -1, -1 };
+        int s = 0;
+        int e = n.length - 1;
+        int m = s + (e - s) / 2;
 
-        if(n.length == 0){
+        if (n.length == 0) {
             return ans;
         }
         ans[0] = findFirst(t, n);
