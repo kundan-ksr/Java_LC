f6bbb86b4cedd03655c1e0293a13e131ccac051d
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-09-06 19:49:38 +0530

    Time: 127 ms (7.27%), Space: 44.1 MB (84.24%) - LeetHub

--- 2717-semi-ordered-permutation/2717-semi-ordered-permutation.java
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.java
@@ -1,26 +1,19 @@
 class Solution {
     public int semiOrderedPermutation(int[] nums) {
-        int opr = 0, j = 0;
-//         finding 1
-        for(int i=0; i<nums.length; i++)    
-            if(nums[i] == 1)    j = i;
-//         operating 1 to start index
-        while(j > 0){
-            nums[j] = nums[j-1];
-            nums[j-1] = 1;
-            opr++;
-            j--;
+        int sInd = -1;
+        int eInd = -1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                sInd = i;
+            }
+            else if(nums[i] == nums.length){
+                eInd = i;
+            }
         }
-//         finding n
-        for(int i=0; i<nums.length; i++)    
-            if(nums[i] == nums.length)    j = i;
-//         operating n to last index
-        while(j < nums.length-1){
-            nums[j] = nums[j+1];
-            nums[j+1] = nums.length;
-            opr++;
-            j++;
-        }   
-        return opr;
+        if(sInd < eInd){
+            return ((sInd) + (nums.length-1-eInd));
+        }else{
+            return ((sInd) + (nums.length-2-eInd));
+        }
     }
 }
