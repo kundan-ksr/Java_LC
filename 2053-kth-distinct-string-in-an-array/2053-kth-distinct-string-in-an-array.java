304c4a4c4334a460b793659acef7d0dbae871264
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-09-13 19:06:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        StringBuilder sb = new StringBuilder();
+        String s;
+        for(int i=0; i<arr.length; i++){
+            boolean isPresent = false;
+            for(int j=0; j<arr.length; j++){
+                if(arr[i].equals(arr[j]) && i != j){
+                    isPresent = true;
+                }
+            }
+            if(!isPresent){
+                sb.append(arr[i]);
+                sb.append(" ");
+            }
+        }
+        s = sb.toString();
+        if(s.length() < k){
+            return "";
+        }
+
+        String[] ans = s.split(" ");
+        return ans[k-1];
+    }
+}
