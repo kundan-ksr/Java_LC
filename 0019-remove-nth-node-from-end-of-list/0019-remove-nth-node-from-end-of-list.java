c9a5be0a5a28868c4e886c127d9efbea30f84c39
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-09-14 19:05:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -8,27 +8,56 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head == null) return head;
 
-        int len = 0;
-        ListNode temp = head;
-        while(temp != null){
-            len++;
-            temp = temp.next;
-        }
-        if(len == 1 && n==1){
-            return null;
-        }
-        int ind = (len-n-1);
+//My Approach-
+
+class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         if(head == null) return head;
+
+//         int len = 0;
+//         ListNode temp = head;
+//         while(temp != null){
+//             len++;
+//             temp = temp.next;
+//         }
+//         if(len == n){
+//             return head.next;
+//         }
+//         int ind = (len-n-1);
         
-        temp = head;
-        while(ind > 0){
-            temp = temp.next;
-            ind--;
+//         temp = head;
+//         while(ind > 0){
+//             temp = temp.next;
+//             ind--;
+//         }
+//         temp.next = temp.next.next;
+//         return head;
+//     }
+
+
+//Optimized approach in one go-
+public static ListNode DeleteNthNodefromEnd(ListNode head, int N) {
+        // Create two pointers, fastp and slowp
+        ListNode fastp = head;
+        ListNode slowp = head;
+
+        // Move the fastp pointer N nodes ahead
+        for (int i = 0; i < N; i++)
+            fastp = fastp.next;
+
+        // If fastp becomes null, the Nth node from the end is the head
+        if (fastp == null)
+            return head.next;
+
+        // Move both pointers until fastp reaches the end
+        while (fastp.next != null) {
+            fastp = fastp.next;
+            slowp = slowp.next;
         }
-        temp.next = temp.next.next;
+
+        // Delete the Nth node from the end
+        slowp.next = slowp.next.next;
         return head;
     }
 }
