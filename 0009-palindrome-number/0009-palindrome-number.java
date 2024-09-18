4de7ef5c458275dc594203a9432bfc8049a694a6
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-09-19 00:01:31 +0530

    Time: 6 ms (30.12%), Space: 44.1 MB (36.44%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,26 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-//My Approach :
-        // String s = Integer.toString(x);
-        // int n = s.length();
-        // for(int i=0; i<n/2; i++){
-        //     if(s.charAt(i) != s.charAt(n-i-1)) return false;
-        // }
-        // return true;
-            
-// no negative number can read as a palindrome
-        if (x < 0) {
-            return false;
+        String s = Integer.toString(x);
+        int len = s.length();
+        for(int i=0; i<len; i++){
+            if(s.charAt(i) != s.charAt(len - i - 1)){
+                return false;
+            }
         }
-
-        int reversed = 0;
-        int actual = x;
-        while (x > 0) {
-            int digit = x % 10;
-            reversed = reversed * 10 + digit;
-            x /= 10;
-        }
-
-        return (actual == reversed);
+        return true;
     }
 }
