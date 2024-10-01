718949b57c2873b2f7d8c817f6b92bc6f6fecc43
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-10-02 01:04:40 +0530

    Time: 0 ms (100%), Space: 41.1 MB (74.74%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,13 +1,15 @@
 class Solution {
     public int largestAltitude(int[] gain) {
+
         int max = 0;
-        int curr = 0;
-        for(int i=0; i<gain.length; i++){
-            curr += gain[i];
-            if(curr >= 0){
-                max = Math.max(curr,max);
+        int current = 0;
+
+        for (int i = 0; i < gain.length; i++) {
+            current += gain[i]; {
+                max = Math.max(max, current);
             }
         }
-        return max;
+        
+        return max;        
     }
 }
