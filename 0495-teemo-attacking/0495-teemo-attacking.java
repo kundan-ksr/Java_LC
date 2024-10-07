f150d89174dc91c4c65fe44463d2a009631b58a0
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-10-07 22:51:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int count = 0;
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+
+            // add duration normally
+            if (timeSeries[i] - timeSeries[i - 1] >= duration) {
+                count += duration;
+            }
+
+            else { // if next attack occurs before current duration ends, include the difference
+
+                count += timeseries[i] - timeseries[i - 1]; // it is done to count the duration only    once, not repeat it
+            }
+
+        }
+        
+        return count + duration;  // include last attack from teemo
+    }
+}
