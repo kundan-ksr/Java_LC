2c0d07156b68cfbf8020944bc5cf864a29f624f4
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2024-12-16 21:14:10 +0530

    Time: 0 ms (100%), Space: 41 MB (10.39%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int mask = start ^ goal;
+        
+        int count = 0;
+        while(mask>0){
+            if((mask & 1) == 1){
+                count++;
+            }
+            mask = mask >> 1;
+        }
+        return count;
+    }
+}
