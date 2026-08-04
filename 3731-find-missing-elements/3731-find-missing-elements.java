4360c7ae3d8370c933eb3ea2cd63357a19074700
kundan ksr. <131528777+kundan-ksr@users.noreply.github.com>
2026-08-04 22:50:24 +0530

    Time: 1 ms (100%), Space: 46.7 MB (80.06%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,19 +1,46 @@
+// class Solution {
+//     public List<Integer> findMissingElements(int[] nums) {
+//         ArrayList<Integer> ans = new ArrayList<>();
+
+//         Arrays.sort(nums);
+//         int curr = nums[0];
+        
+//         for(int i=0; i<nums.length; i++){
+//             if(nums[i] != curr){
+//                 while(curr < nums[i]){
+//                     ans.add(curr);
+//                     curr++;
+//                 }
+//             }
+//             curr++;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        ArrayList<Integer> ans = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        int max = nums[0];
+        int min = nums[0];
 
-        Arrays.sort(nums);
-        int curr = nums[0];
-        
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] != curr){
-                while(curr < nums[i]){
-                    ans.add(curr);
-                    curr++;
-                }
-            }
-            curr++;
+        for(int i : nums){
+            max = Math.max(i, max);
+            min = Math.min(i, min);
+        }
+
+        int[] arr = new int[max + 1];
+        for(int i : nums){
+            arr[i]++;
+        }
+
+        for(int i=min;i<max;i++){
+            if(arr[i] == 0)
+                ans.add(i);
         }
         return ans;
+
+
     }
 }
